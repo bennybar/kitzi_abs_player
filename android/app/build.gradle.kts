3c4160e7af388a13a51cd0ab03f6277fcf20880d
bbarak@kenes.com <bbarak@kenes.com>
2025-09-03 15:07:39 +0300

    1

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -20,6 +20,7 @@ android {
         jvmTarget = JavaVersion.VERSION_11.toString()
     }
 
+
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
         applicationId = "com.bennybar.kitzi"
